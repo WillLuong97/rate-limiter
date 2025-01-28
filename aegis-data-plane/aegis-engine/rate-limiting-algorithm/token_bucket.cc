7e5acfc58bffb05549b43208e464546007eedf24
Tri (Will) Luong <willie@Tris-MBP.attlocal.net>
2025-01-27 21:52:48 -0600

    Added token_bucket.h header file with the intial functions to mplement the token bucket algorithm
    Added header file for all the function to implement the token bucket algorithm
    
    Added C++ redis cache client
    
    Update the readme.md file with more information about the design of aegis, adding information about CAP theorem, core entities, and system interface
    
    Updated readme with more information about the high level architecture of the rate limiter

---
+++ aegis-data-plane/aegis-engine/rate-limiting-algorithm/token_bucket.cc
@@ -0,0 +1,12 @@
+#include "token_bucket.h"
+#include <iostream>
+
+using namespace std;  
+
+string* 
+TokenBucket::createBucket() {
+   //Create a bucket in the redis cache to store our tokens
+   
+
+}
+
---
+++ aegis-data-plane/aegis-engine/redis/redis_main.cc
@@ -0,0 +1,5 @@
+//This is the main runner file for redis to start up a redis cache and running api call to interact with redis using the C++ client 
+
+
+
+
